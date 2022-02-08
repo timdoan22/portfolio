dceba3d4ab8772a478016b15e5da22b43dd9fb32
timdoan22 <timdoan01@gmail.com>
2022-02-09 00:00:00 +0900

    Added JS functionality for collapsed hamburger navbar; added main.js script to index.html

---
+++ script/main.js
@@ -0,0 +1,13 @@
+const toggle = document.querySelector('.nav__wrapper__toggle');
+const ul = document.querySelector('.nav__wrapper__ul');
+document.querySelector('button').addEventListener('click', formRedirect)
+
+// add click event on toggle
+
+toggle.addEventListener('click', () => {
+    ul.classList.toggle('nav__active');
+});
+
+function formRedirect() {
+    window.location.href ="#";
+}
