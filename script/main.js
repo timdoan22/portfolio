ff349e925d28069d975e2f82e91c9b1f72a2441a
timdoan22 <timdoan01@gmail.com>
2022-03-01 00:00:00 +0900

    Added form functionality to main.js script

--- script/main.js
+++ script/main.js
@@ -2,9 +2,14 @@ console.log("working")
 
 const toggle = document.querySelector('.nav__wrapper__toggle');
 const ul = document.querySelector('.nav__wrapper__ul');
+document.querySelector('button').addEventListener('click', formRedirect);
 
 
 // add click event on toggle
 toggle.addEventListener('click', () => {
     ul.classList.toggle('nav__active');
 });
+
+function formRedirect() {
+    window.location.href ="#";
+}
